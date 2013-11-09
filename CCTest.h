fca8e5ab56d9b4a4502befcc2b5d2034345a17db
kevin doherty <kvdoherty@gmail.com>
2013-11-09 08:23:50 -0500

    add test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test 2
+//
+//  Created by Kevin Doherty on 11/9/13.
+//  Copyright (c) 2013 Kevin Doherty. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test 2
+//
+//  Created by Kevin Doherty on 11/9/13.
+//  Copyright (c) 2013 Kevin Doherty. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
